655b317199e8c8eb484b08fcf0c5b533c2a2e888
Bhimesh Yadav <94525668+uniquebhim@users.noreply.github.com>
2024-03-11 22:05:18 +0530

    Update mail.js

--- controller/mail.js
+++ controller/mail.js
@@ -1,32 +1,32 @@
-const nodemailer = require('nodemailer');
-const mailGun = require('nodemailer-mailgun-transport');
+// const nodemailer = require('nodemailer');
+// const mailGun = require('nodemailer-mailgun-transport');
 
-const auth = {
-    auth: {
-        api_key: process.env.API_KEY , // TODO: Replace with your mailgun API KEY
-        domain: process.env.DOMAIN  // TODO: Replace with your mailgun DOMAIN
-    }
-};
+// const auth = {
+//     auth: {
+//         api_key: process.env.API_KEY , // TODO: Replace with your mailgun API KEY
+//         domain: process.env.DOMAIN  // TODO: Replace with your mailgun DOMAIN
+//     }
+// };
 
-const transporter = nodemailer.createTransport(mailGun(auth));
+// const transporter = nodemailer.createTransport(mailGun(auth));
 
 
-const sendMail = (email, subject, text) => {
-    const mailOptions = {
-        from: email, 
-        to: "as77jadoun@gmail.com", 
-        subject,
-        text
-    };
+// const sendMail = (email, subject, text) => {
+//     const mailOptions = {
+//         from: email, 
+//         to: "as77jadoun@gmail.com", 
+//         subject,
+//         text
+//     };
 
-    transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-            console.log("ERROR!! :" ,err);
-        }else{
-            console.log("Message sent!!!");
-        }
+//     transporter.sendMail(mailOptions, function (err, data) {
+//         if (err) {
+//             console.log("ERROR!! :" ,err);
+//         }else{
+//             console.log("Message sent!!!");
+//         }
         
-    });
-}
+//     });
+// }
 
-module.exports = sendMail;
+// module.exports = sendMail;
